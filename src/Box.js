025983cc8957eb50a0929e9e9c2df592f9c61559
amariwawa <aumosen10@gmail.com>
2025-10-22 16:23:57 +0100

    Fix formatting and import statements in Box.js

--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Box.css';
 
-import "./Box.css"
-
-export const Box = ({value, onClick }) => {
-  const style = value === "X" ? "Box X" : "Box O"
+export const Box = ({ value, onClick }) => {
+  const style = value === "X" ? "Box X" : value === "O" ? "Box O" : "Box";
 
   return (
-    <button className={style} onClick={onClick}>{value}</button>
-  )
-}
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
